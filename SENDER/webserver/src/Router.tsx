994ef7206ab93938a4ed7fb7b068806f61b80fee
marcu <marcussoh38@gmail.com>
2025-02-23 20:11:41 +0800

    Update working directory

--- SENDER/webserver/src/Router.tsx
+++ SENDER/webserver/src/Router.tsx
@@ -1,13 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//   },
+// ]);
 
+
+const router = createHashRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+    },
+]);
 export function Router() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
